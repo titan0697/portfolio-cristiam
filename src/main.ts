3b831ef4b9b12eea9b2870b95b4b14711e146568
titan6506 <69824130+titan6506@users.noreply.github.com>
2026-05-04 15:45:48 -0600

    ajustanfo publicado

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+const redirect = sessionStorage['redirect'];
+
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
